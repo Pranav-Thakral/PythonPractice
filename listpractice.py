a1d883a869ed136ea9fb15579c2744b5ac1815bd
pranav <thakralpranav23@gmail.com>
2023-08-03 10:44:27 -0700

    practice

---
+++ listpractice.py
@@ -0,0 +1,6 @@
+shopping_list=['milk','pasta','eggs','spam','rice','cookies']
+print(shopping_list)
+shopping_list+=['cookies']
+print(shopping_list)
+shopping_list.append ('cream')
+print(shopping_list)
