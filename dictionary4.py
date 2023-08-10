cfe7f27ebf8d680f177b1b3849addf39701888e8
pranav <thakralpranav23@gmail.com>
2023-08-09 19:47:48 -0700

    practice

---
+++ dictionary4.py
@@ -0,0 +1,11 @@
+my_dict={"india":"delhi",
+         "pakistan":"islamabad",
+         "UAE":"Abu DAbhi"}
+print(my_dict)
+
+my_dict["india"]="new delhi"
+print(my_dict)
+my_dict["germany"]="berlin"
+print(my_dict)
+for country in my_dict:
+    print(country)
