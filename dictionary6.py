aa239fa21b6d9c4105974cb3d8649c8118e8b7a3
pranav <thakralpranav23@gmail.com>
2023-08-09 19:50:36 -0700

    practice

---
+++ dictionary6.py
@@ -0,0 +1,7 @@
+#to check if value is present in a dictionary
+dict={'a':100,'b':200,'c':300}
+print(dict)
+if 200 in dict.values():
+    print("200 is present in dict")
+else:
+    print("200 is not present")
